d48d5e996ebe4e8dd6a8b4d11123e0fefcc6117c
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-04-10 09:24:16 +0900

    API処理追加

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/api/SWService.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/api/SWService.kt
@@ -2,6 +2,10 @@ package com.sw.sw_api_kotlin_project.api
 
 import com.sw.sw_api_kotlin_project.model.films.Films
 import com.sw.sw_api_kotlin_project.model.peple.People
+import com.sw.sw_api_kotlin_project.model.planet.Planet
+import com.sw.sw_api_kotlin_project.model.species.Species
+import com.sw.sw_api_kotlin_project.model.starships.Starships
+import com.sw.sw_api_kotlin_project.model.vehicles.Vehicles
 import retrofit2.Response
 import retrofit2.http.GET
 
@@ -14,11 +18,34 @@ interface SWService {
     @GET("people")
     suspend fun people(): Response<People>
 
-
     /**
      * 映画情報取得
      */
     @GET("films")
     suspend fun films(): Response<Films>
+
+    /**
+     * 惑星取得
+     */
+    @GET("planets")
+    suspend fun planets(): Response<Planet>
+
+    /**
+     * 種族取得
+     */
+    @GET("species")
+    suspend fun species(): Response<Species>
+
+    /**
+     * 宇宙船取得
+     */
+    @GET("starships")
+    suspend fun starships(): Response<Starships>
+
+    /**
+     * 車両取得
+     */
+    @GET("vehicles")
+    suspend fun vehicles(): Response<Vehicles>
 
 }
