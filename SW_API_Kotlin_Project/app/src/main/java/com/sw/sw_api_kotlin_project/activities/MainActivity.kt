3f84d0c8af16c75e9d03bb919e49823658b59ac2
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-04-10 16:50:10 +0900

    ホームフラグメント追加

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/activities/MainActivity.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/activities/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.sw.sw_api_kotlin_project.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sw.sw_api_kotlin_project.R
+import com.sw.sw_api_kotlin_project.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun initViews() {
+        // 何もしない
+    }
+
+
+    override fun initData() {
+        // 何もしない
+    }
+
 }
