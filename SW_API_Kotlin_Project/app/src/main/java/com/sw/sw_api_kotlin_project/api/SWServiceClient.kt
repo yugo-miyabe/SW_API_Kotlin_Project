d31e0bb2442b075aaacd63faefd842b72c11664f
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-04-11 10:17:05 +0900

    jsonコンバーター追加

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/api/SWServiceClient.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/api/SWServiceClient.kt
@@ -1,12 +1,16 @@
 package com.sw.sw_api_kotlin_project.api
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
 
 class SWServiceClient {
     companion object {
         fun getService(): SWService {
             val builder = Retrofit.Builder()
-                .baseUrl("https://swapi.dev/api/")
+                .baseUrl("https://swapi.dev/")
+                .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
                 .build()
             return builder.create(SWService::class.java)
         }
