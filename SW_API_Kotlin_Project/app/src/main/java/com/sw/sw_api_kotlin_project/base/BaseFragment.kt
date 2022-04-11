2fdc7c76a22cfc1d94aab978cbf973c95b968388
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-04-11 20:23:55 +0900

    ログを出力する処理を追加

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/base/BaseFragment.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/base/BaseFragment.kt
@@ -1,16 +1,67 @@
 package com.sw.sw_api_kotlin_project.base
 
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 
 open class BaseFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(this::class.java.simpleName, Thread.currentThread().stackTrace[2].methodName)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Log.d(this::class.java.simpleName, Thread.currentThread().stackTrace[2].methodName)
+
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         addObservers()
     }
 
-    open fun addObservers() {}
+    override fun onStart() {
+        super.onStart()
+        Log.d(this::class.java.simpleName, Thread.currentThread().stackTrace[2].methodName)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(this::class.java.simpleName, Thread.currentThread().stackTrace[2].methodName)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(this::class.java.simpleName, Thread.currentThread().stackTrace[2].methodName)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d(this::class.java.simpleName, Thread.currentThread().stackTrace[2].methodName)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(this::class.java.simpleName, Thread.currentThread().stackTrace[2].methodName)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.d(this::class.java.simpleName, Thread.currentThread().stackTrace[2].methodName)
+    }
+
+    open fun addObservers() {
+        Log.d(this::class.java.simpleName, Thread.currentThread().stackTrace[2].methodName)
+    }
+
 }
