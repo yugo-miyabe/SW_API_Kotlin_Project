d26246b5ae6de77ab4d235b794a9fd92ffa2aeae
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-04-11 10:16:12 +0900

    APIのリポジトリ追加

---
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/repository/APIRepository.kt
@@ -0,0 +1,19 @@
+package com.sw.sw_api_kotlin_project.repository
+
+import com.sw.sw_api_kotlin_project.model.error.APIError
+import com.sw.sw_api_kotlin_project.utils.Result
+import kotlinx.serialization.json.Json
+import retrofit2.Response
+
+class APIRepository {
+    fun <T> getResponse(response: Response<T>): Result<T> {
+        return if (response.isSuccessful) {
+            val body = response.body()!!
+            Result.Success(body)
+        } else {
+            val errorBody = response.errorBody()!!.string()
+            val errorResponse = Json.decodeFromString(APIError.serializer(), errorBody)
+            Result.Error(errorResponse)
+        }
+    }
+}
