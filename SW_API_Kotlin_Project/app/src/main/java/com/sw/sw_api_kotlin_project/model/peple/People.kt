b15143e657689088ad6452b8a239186d966fffdb
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-04-14 13:45:15 +0900

    データモデルのプロパティの順序変更

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/model/peple/People.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/model/peple/People.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class People(
     val count: Int,
-    val next: String,
+    val next: String?,
     val previous: String?,
     val results: List<Result>
 )
