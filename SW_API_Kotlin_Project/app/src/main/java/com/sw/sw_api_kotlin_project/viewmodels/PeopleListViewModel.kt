7d21d1019ce53083202f8a36299d98df820593db
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-04-14 10:27:04 +0900

    People画面のViewModelを作成

---
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/viewmodels/PeopleListViewModel.kt
@@ -0,0 +1,47 @@
+package com.sw.sw_api_kotlin_project.viewmodels
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.sw.sw_api_kotlin_project.api.SWServiceClient
+import com.sw.sw_api_kotlin_project.base.BaseViewModel
+import com.sw.sw_api_kotlin_project.model.peple.People
+import com.sw.sw_api_kotlin_project.repository.APIRepository
+import com.sw.sw_api_kotlin_project.utils.Result
+import kotlinx.coroutines.launch
+
+class PeopleListViewModel(private val apiRepository: APIRepository) : BaseViewModel() {
+    private val _people = MutableLiveData<People>()
+    val people = _people
+
+    fun getPeopleAPI() {
+        viewModelScope.launch {
+            val api = SWServiceClient.getService()
+            val response = api.people()
+            when (val result = apiRepository.getResponse(response)) {
+                is Result.Success -> {
+                    _people.value = result.data
+                }
+                is Result.Error -> {
+                    // TODO エラーハンドリング
+                    result.type
+                }
+                else -> {
+                    // 何もしない
+                }
+            }
+        }
+    }
+}
+
+class PeopleListViewModelFactory(private val apiRepository: APIRepository) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(APIRootViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return PeopleListViewModel(apiRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
