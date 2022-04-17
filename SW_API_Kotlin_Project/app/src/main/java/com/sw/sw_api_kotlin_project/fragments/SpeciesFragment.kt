8563981273a099a7be2fb9f21515584f6d6de159
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-04-17 23:24:41 +0900

    AdapterをFragmentに反映

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/fragments/SpeciesFragment.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/fragments/SpeciesFragment.kt
@@ -4,16 +4,51 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.sw.sw_api_kotlin_project.R
+import com.sw.sw_api_kotlin_project.adapters.SpeciesAdapter
+import com.sw.sw_api_kotlin_project.base.BaseFragment
+import com.sw.sw_api_kotlin_project.repository.APIRepository
+import com.sw.sw_api_kotlin_project.viewmodels.SpeciesViewModel
+import com.sw.sw_api_kotlin_project.viewmodels.SpeciesViewModelFactory
 
-class SpeciesFragment : Fragment() {
+class SpeciesFragment : BaseFragment() {
+
+    private lateinit var viewModel: SpeciesViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(
+            this,
+            SpeciesViewModelFactory(
+                APIRepository()
+            )
+        )[SpeciesViewModel::class.java]
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_species, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.getSpeciesAPI()
+    }
+
+    override fun addObservers() {
+        super.addObservers()
+        viewModel.species.observe(viewLifecycleOwner) {
+            val adapter = SpeciesAdapter(it.species)
+            val recyclerView = view?.findViewById<RecyclerView>(R.id.species_recycler)
+            recyclerView!!.adapter = adapter
+            recyclerView.layoutManager = LinearLayoutManager(context)
+        }
+
+    }
+
 }
