21588cd801988a170dbddcbf90a92ffdb4d5f6f1
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-04-17 13:52:46 +0900

    film画面を生成

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/fragments/FilmsFragment.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/fragments/FilmsFragment.kt
@@ -4,17 +4,48 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.sw.sw_api_kotlin_project.R
+import com.sw.sw_api_kotlin_project.adapters.FilmsAdapter
+import com.sw.sw_api_kotlin_project.base.BaseFragment
+import com.sw.sw_api_kotlin_project.repository.APIRepository
+import com.sw.sw_api_kotlin_project.viewmodels.FilmsListViewModel
+import com.sw.sw_api_kotlin_project.viewmodels.FilmsListViewModelFactory
 
+class FilmsFragment : BaseFragment() {
 
-class FilmsFragment : Fragment() {
+    private lateinit var viewModel: FilmsListViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(
+            this,
+            FilmsListViewModelFactory(APIRepository())
+        )[FilmsListViewModel::class.java]
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_films, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.getFilmsAPI()
+    }
+
+    override fun addObservers() {
+        super.addObservers()
+        viewModel.films.observe(viewLifecycleOwner) {
+            val adapter = FilmsAdapter(it.films)
+            val recyclerView = view?.findViewById<RecyclerView>(R.id.film_recycler)
+            recyclerView!!.adapter = adapter
+            recyclerView.layoutManager = LinearLayoutManager(context)
+        }
     }
 
 }
---
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/viewmodels/FilmsListViewModel.kt
@@ -0,0 +1,66 @@
+package com.sw.sw_api_kotlin_project.viewmodels
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.sw.sw_api_kotlin_project.api.SWServiceClient
+import com.sw.sw_api_kotlin_project.base.BaseViewModel
+import com.sw.sw_api_kotlin_project.model.films.FilmsRoot
+import com.sw.sw_api_kotlin_project.repository.APIRepository
+import com.sw.sw_api_kotlin_project.utils.Result
+import kotlinx.coroutines.launch
+
+class FilmsListViewModel(private val apiRepository: APIRepository) : BaseViewModel() {
+
+    private val _films = MutableLiveData<FilmsRoot>()
+    val films = _films
+
+    fun getFilmsAPI() {
+        viewModelScope.launch {
+            val api = SWServiceClient.getService()
+            val response = api.films()
+            when (val result = apiRepository.getResponse(response)) {
+                is Result.Success -> {
+                    _films.value = result.data
+                }
+                is Result.Error -> {
+                    result.type
+                }
+            }
+        }
+
+        /*
+            fun getPeopleAPI() {
+            viewModelScope.launch {
+                val api = SWServiceClient.getService()
+                val response = api.people()
+                when (val result = apiRepository.getResponse(response)) {
+                    is Result.Success -> {
+                        _people.value = result.data
+                    }
+                    is Result.Error -> {
+                        // TODO エラーハンドリング
+                        result.type
+                    }
+                    else -> {
+                        // 何もしない
+                    }
+                }
+            }
+        }
+         */
+
+    }
+}
+
+class FilmsListViewModelFactory(private val apiRepository: APIRepository) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(FilmsListViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return FilmsListViewModel(apiRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
