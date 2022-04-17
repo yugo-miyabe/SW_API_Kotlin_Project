bbfd170ee56799c61f82888af1eb2b16e403e653
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-04-18 00:07:29 +0900

    Adapter作成

---
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/adapters/VehiclesAdapter.kt
@@ -0,0 +1,38 @@
+package com.sw.sw_api_kotlin_project.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.sw.sw_api_kotlin_project.R
+import com.sw.sw_api_kotlin_project.model.vehicles.Vehicles
+
+class VehiclesAdapter(private val vehicleList: List<Vehicles>) :
+    RecyclerView.Adapter<VehiclesAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val vehicleName = view.findViewById<TextView>(R.id.vehicle_name_text)
+        private val modelName = view.findViewById<TextView>(R.id.model_name_text)
+        private val manufacturerName = view.findViewById<TextView>(R.id.manufacturer_name_text)
+
+        fun bind(vehicles: Vehicles) {
+            vehicleName.text = vehicles.name
+            modelName.text = vehicles.model
+            manufacturerName.text = vehicles.manufacturer
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_vehicles, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(vehicleList[position])
+    }
+
+    override fun getItemCount() = vehicleList.size
+}
