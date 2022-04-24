ecf616dadf5d90598b0d0910c6c80ae8d1dd33c0
yugo-miyabe <yuni42yuyuyu@gmail.com>
2022-04-24 12:46:05 +0900

    Fragmentをenumで管理するよう変更

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/utils/Constants.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/utils/Constants.kt
@@ -1,30 +1,14 @@
 package com.sw.sw_api_kotlin_project.utils
 
-import androidx.fragment.app.Fragment
-import com.sw.sw_api_kotlin_project.fragments.FilmsFragment
-import com.sw.sw_api_kotlin_project.fragments.PeopleListFragment
-import com.sw.sw_api_kotlin_project.fragments.PlanetsFragment
-import com.sw.sw_api_kotlin_project.fragments.SpeciesFragment
-import com.sw.sw_api_kotlin_project.fragments.StarShipsFragment
-import com.sw.sw_api_kotlin_project.fragments.VehiclesFragment
-
 object Constants {
-    val API_ROOT_LIST: List<String> = listOf(
-        "people",
-        "planets",
-        "films",
-        "species",
-        "vehicles",
-        "starships"
-    )
 
-    val FRAGMENT_LIST: Map<String, Fragment> = mapOf(
-        API_ROOT_LIST[0] to PeopleListFragment(),
-        API_ROOT_LIST[1] to PlanetsFragment(),
-        API_ROOT_LIST[2] to FilmsFragment(),
-        API_ROOT_LIST[3] to SpeciesFragment(),
-        API_ROOT_LIST[4] to VehiclesFragment(),
-        API_ROOT_LIST[5] to StarShipsFragment()
-    )
+    enum class Root(val title: String) {
+        People("登場人物"),
+        Planets("惑星"),
+        Films("映画"),
+        Species("種族"),
+        Vehicles("車両"),
+        StarShips("宇宙船"),
+    }
 
 }
