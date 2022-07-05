4f0710452fb003c5dea3250ccc4e02142739d2f5
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-07-05 23:51:44 +0900

    ViewModelのプロバイダー追加

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/fragments/PeopleDetailsFragment.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/fragments/PeopleDetailsFragment.kt
@@ -4,20 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.sw.sw_api_kotlin_project.R
+import androidx.lifecycle.ViewModelProvider
+import com.sw.sw_api_kotlin_project.base.BaseFragment
+import com.sw.sw_api_kotlin_project.databinding.FragmentPeopleDetailsBinding
+import com.sw.sw_api_kotlin_project.viewmodels.PeopleDetailsFactory
+import com.sw.sw_api_kotlin_project.viewmodels.PeopleDetailsViewModel
 
 
-class PeopleDetailsFragment : Fragment() {
+class PeopleDetailsFragment : BaseFragment() {
+    private lateinit var viewModel: PeopleDetailsViewModel
+    private var _binding: FragmentPeopleDetailsBinding? = null
+    private val binding get() = checkNotNull(_binding)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(
+            this,
+            PeopleDetailsFactory()
+        )[PeopleDetailsViewModel::class.java]
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_people_details, container, false)
+    ): View {
+        _binding = FragmentPeopleDetailsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
 }
