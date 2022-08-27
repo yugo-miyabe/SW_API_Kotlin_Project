83c1b9824e361afea6046b156fbbbe691875ca17
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-08-27 21:57:48 +0900

    型変更

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/data/dao/PeopleSWAPIDao.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/data/dao/PeopleSWAPIDao.kt
@@ -3,16 +3,16 @@ package com.sw.sw_api_kotlin_project.data.dao
 import com.sw.sw_api_kotlin_project.data.model.People
 import com.sw.sw_api_kotlin_project.data.model.Results
 import com.sw.sw_api_kotlin_project.network.SWService
+import retrofit2.Response
 
 class PeopleSWAPIDao(private val swService: SWService) {
 
-/*
-    suspend fun fetchPeople(page: Int): Results<People> {
-        return swService.fetchPeople(page)
+    suspend fun fetchPeople(page: Int): Response<Results<People>> {
+        return swService.getPeople(page)
     }
 
-    suspend fun searchPeople(page: Int, searchString: String): Results<People> {
-        return swService.fetchPeopleSearchPage(searchString, page)
+    suspend fun searchPeople(page: Int, searchString: String): Response<Results<People>> {
+        return swService.getPeopleSearchPage(searchString, page)
     }
 
 
@@ -20,5 +20,4 @@ class PeopleSWAPIDao(private val swService: SWService) {
         return swService.getPeopleByUrl(peopleUrl)
     }
 
-*/
 }
