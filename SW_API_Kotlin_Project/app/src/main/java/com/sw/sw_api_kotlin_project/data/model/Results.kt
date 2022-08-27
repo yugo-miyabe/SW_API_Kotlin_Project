42c988095002a59a23e232cf6b11ee714645420b
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-08-27 21:55:51 +0900

    Serializableアノテーション追加

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/data/model/Results.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/data/model/Results.kt
@@ -1,9 +1,12 @@
 package com.sw.sw_api_kotlin_project.data.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Results<T>(
     val count: Int,
-    val next: String,
-    val previous: String,
+    val next: String?,
+    val previous: String?,
     val results: List<T>,
 )
 
