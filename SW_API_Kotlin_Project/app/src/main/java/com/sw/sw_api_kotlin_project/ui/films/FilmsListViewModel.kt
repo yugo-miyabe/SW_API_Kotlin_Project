77f413aed0d14b60f1a11d2589ec9a77e878ec0c
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-08-29 19:26:20 +0900

    映画情報取得処理を修正

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/ui/films/FilmsListViewModel.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/ui/films/FilmsListViewModel.kt
@@ -5,12 +5,14 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.liveData
 import com.sw.sw_api_kotlin_project.base.BaseViewModel
 import com.sw.sw_api_kotlin_project.repository.FilmsRepository
+import com.sw.sw_api_kotlin_project.utils.PageType
 import com.sw.sw_api_kotlin_project.utils.Resource
 import kotlinx.coroutines.Dispatchers
 
 class FilmsListViewModel(private val filmsRepository: FilmsRepository) : BaseViewModel() {
 
-    fun getFilms(page: Int = 1) = liveData(Dispatchers.IO) {
+    fun getFilms(pageType: PageType) = liveData(Dispatchers.IO) {
+        pageFormat(pageType)
         emit(Resource.loading(data = null))
         try {
             val response = filmsRepository.getFilms(page)
