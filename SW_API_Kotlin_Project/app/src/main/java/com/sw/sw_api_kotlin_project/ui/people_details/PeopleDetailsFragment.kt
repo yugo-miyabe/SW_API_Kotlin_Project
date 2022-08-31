3287ec857d211273e2d1c495271a29c5d4010a0f
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-08-31 18:15:23 +0900

    詳細画面追加

---
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/ui/people_details/PeopleDetailsFragment.kt
@@ -0,0 +1,38 @@
+package com.sw.sw_api_kotlin_project.ui.people_details
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.sw.sw_api_kotlin_project.base.BaseFragment
+import com.sw.sw_api_kotlin_project.databinding.FragmentPeopleDetailsBinding
+
+class PeopleDetailsFragment : BaseFragment() {
+    private lateinit var viewModel: PeopleDetailsViewModel
+    private var _binding: FragmentPeopleDetailsBinding? = null
+    private val binding get() = checkNotNull(_binding)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(
+            this,
+            PeopleDetailsFactory()
+        )[PeopleDetailsViewModel::class.java]
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPeopleDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+}
