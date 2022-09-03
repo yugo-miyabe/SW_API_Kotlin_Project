cf49a7ee790caf61e918b263d592658d540f7ba5
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-09-03 23:32:15 +0900

    リネーム

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/adapters/PeopleAdapter.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/adapters/PeopleAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.sw.sw_api_kotlin_project.R
 import com.sw.sw_api_kotlin_project.data.model.People
@@ -11,11 +12,15 @@ import com.sw.sw_api_kotlin_project.data.model.People
 class PeopleAdapter(private val peopleList: List<People>) :
     RecyclerView.Adapter<PeopleAdapter.ViewHolder>() {
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    class ViewHolder(private val view: View) :
+        RecyclerView.ViewHolder(view) {
         private val fullName = view.findViewById<TextView>(R.id.full_name_text)
 
         fun bind(people: People) {
             fullName.text = people.name
+            view.setOnClickListener {
+                view.findNavController().navigate(R.id.action_nav_people_to_nav_people_detail)
+            }
         }
     }
 
@@ -23,7 +28,7 @@ class PeopleAdapter(private val peopleList: List<People>) :
         val view =
             LayoutInflater.from(parent.context).inflate(R.layout.item_people, parent, false)
 
-        return ViewHolder(view);
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
