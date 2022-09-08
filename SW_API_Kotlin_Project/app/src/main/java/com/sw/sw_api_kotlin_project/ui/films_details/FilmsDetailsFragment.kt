3ae09fb927b8cb883bd916de386982022dcd73a1
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-09-09 00:58:40 +0900

    値を反映

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/ui/films_details/FilmsDetailsFragment.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/ui/films_details/FilmsDetailsFragment.kt
@@ -5,13 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.navArgs
 import com.sw.sw_api_kotlin_project.base.BaseFragment
+import com.sw.sw_api_kotlin_project.data.model.Films
 import com.sw.sw_api_kotlin_project.databinding.FragmentFilmsDetailsBinding
 
 class FilmsDetailsFragment : BaseFragment() {
     private lateinit var viewModel: FilmsDetailsViewModel
     private var _binding: FragmentFilmsDetailsBinding? = null
     private val binding get() = checkNotNull(_binding)
+    private val args: FilmsDetailsFragmentArgs by navArgs()
+    private lateinit var films: Films
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +29,16 @@ class FilmsDetailsFragment : BaseFragment() {
     ): View {
         _binding = FragmentFilmsDetailsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun initView() {
+        super.initView()
+        films = args.films
+        films.run {
+            binding.titleText.text = title
+            binding.releaseDateText.text = releaseDate
+            binding.openingCrawlText.text = openingCrawl
+        }
     }
 
     override fun onDestroy() {
