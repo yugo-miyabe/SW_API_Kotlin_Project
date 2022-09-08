f044e4957d927a80c1bb137625eba2b7889bad02
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-09-09 00:25:32 +0900

    ViewBidingのベースを実装

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/ui/films_details/FilmsDetailsFragment.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/ui/films_details/FilmsDetailsFragment.kt
@@ -1,7 +1,35 @@
 package com.sw.sw_api_kotlin_project.ui.films_details
 
-class FilmsDetailsFragment {
-    private lateinit var viewModel: FilmsDetailsViewModel
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.sw.sw_api_kotlin_project.base.BaseFragment
+import com.sw.sw_api_kotlin_project.databinding.FragmentFilmsDetailsBinding
 
+class FilmsDetailsFragment : BaseFragment() {
+    private lateinit var viewModel: FilmsDetailsViewModel
+    private var _binding: FragmentFilmsDetailsBinding? = null
+    private val binding get() = checkNotNull(_binding)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this)[FilmsDetailsViewModel::class.java]
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFilmsDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
 
 }
