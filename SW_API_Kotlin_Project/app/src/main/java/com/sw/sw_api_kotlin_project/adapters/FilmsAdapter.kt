86cb0fb0bf65cd72dabd978ed27ac324e6cd9452
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-09-09 00:46:34 +0900

    セルをタップすると画面遷移する処理を追加

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/adapters/FilmsAdapter.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/adapters/FilmsAdapter.kt
@@ -8,21 +8,24 @@ import androidx.recyclerview.widget.RecyclerView
 import com.sw.sw_api_kotlin_project.R
 import com.sw.sw_api_kotlin_project.data.model.Films
 
-class FilmsAdapter(private val filmsList: List<Films>) :
+class FilmsAdapter(private val filmsList: List<Films>, private val onClick: (Films) -> Unit) :
     RecyclerView.Adapter<FilmsAdapter.ViewHolder>() {
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    class ViewHolder(private val view: View, private val onClick: (Films) -> Unit) :
+        RecyclerView.ViewHolder(view) {
         private val move = view.findViewById<TextView>(R.id.move_name)
         fun bind(films: Films) {
             move.text = films.title
+            view.setOnClickListener {
+                onClick(films)
+            }
         }
-
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_films, parent, false)
 
-        return ViewHolder(view)
+        return ViewHolder(view, onClick)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
