02009785bc097f5046e665695e9ac2992bd2ff08
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-09-11 22:26:04 +0900

    MainActivityをbinding

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/activities/MainActivity.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/activities/MainActivity.kt
@@ -3,19 +3,22 @@ package com.sw.sw_api_kotlin_project.activities
 import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.sw.sw_api_kotlin_project.R
 import com.sw.sw_api_kotlin_project.base.BaseActivity
+import com.sw.sw_api_kotlin_project.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity() {
 
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val navController = findNavController(R.id.nav_host_fragment)
-        val navigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        setupWithNavController(navigation, navController)
+        setupWithNavController(binding.bottomNavigation, navController)
     }
 
     override fun initViews() {
@@ -25,6 +28,11 @@ class MainActivity : BaseActivity() {
 
     override fun initData() {
         // 何もしない
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
     }
 
 }
