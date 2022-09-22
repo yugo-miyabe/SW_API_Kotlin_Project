c62c18e853dfab35f8e859ef5d0a87c7bb044961
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-09-22 17:30:41 +0900

    Dao追加

---
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/data/database/FavoriteDao.kt
@@ -0,0 +1,21 @@
+package com.sw.sw_api_kotlin_project.data.database
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface FavoriteDao {
+    @Insert
+    suspend fun insert(favorite: Favorite)
+
+    @Delete
+    suspend fun delete(favorite: Favorite)
+
+    @Query("DELETE FROM favorite_table")
+    suspend fun clear()
+
+    @Query("SELECT * FROM favorite_table WHERE id = :key")
+    suspend fun get(key: Long): Favorite?
+}
