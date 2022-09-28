acb242e338f9b8c2a12339bfa69e915b1e3a969d
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-09-28 18:10:24 +0900

    リポジトリにAPIを取得する処理を追加

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/repository/PlanetRepository.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/repository/PlanetRepository.kt
@@ -3,6 +3,6 @@ package com.sw.sw_api_kotlin_project.repository
 import com.sw.sw_api_kotlin_project.network.SWService
 
 class PlanetRepository(private val swService: SWService) {
-
-    suspend fun getPlanets(page: Int) = swService.getPlanets(page)
+    suspend fun getPlanets(page: Int) = swService.getPlanets(page = page)
+    suspend fun getPlanetsSearch(search: String) = swService.getPlanetsSearch(search = search)
 }
