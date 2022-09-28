97b61b97def87cecfe36fa8a143acdb529369c90
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-09-28 12:01:44 +0900

    検索画面を作成

---
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/ui/search/SearchFragment.kt
@@ -0,0 +1,26 @@
+package com.sw.sw_api_kotlin_project.ui.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sw.sw_api_kotlin_project.base.BaseFragment
+import com.sw.sw_api_kotlin_project.databinding.FragmentSearchBinding
+
+
+class SearchFragment : BaseFragment() {
+    private lateinit var viewModel: SearchViewModel
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = checkNotNull(_binding)
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
