210bd4d4071dfa0a3e94f1bff19a2fa22eab7595
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-09-29 00:40:12 +0900

    Adapterの基盤追加

---
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/adapters/SearchAdapter.kt
@@ -0,0 +1,24 @@
+package com.sw.sw_api_kotlin_project.adapters
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class SearchAdapter() : RecyclerView.Adapter<SearchAdapter.ViewHolder>() {
+
+    class ViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
