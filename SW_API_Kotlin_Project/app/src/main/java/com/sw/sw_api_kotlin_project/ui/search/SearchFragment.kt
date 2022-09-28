88cfeb5d3c0260bdb2c9ff9c013ad91c577836c9
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-09-29 00:12:53 +0900

    検索画面の基盤を作成

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/ui/search/SearchFragment.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/ui/search/SearchFragment.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.sw.sw_api_kotlin_project.api.SWServiceClient
 import com.sw.sw_api_kotlin_project.base.BaseFragment
 import com.sw.sw_api_kotlin_project.databinding.FragmentSearchBinding
+import com.sw.sw_api_kotlin_project.repository.FilmsRepository
+import com.sw.sw_api_kotlin_project.repository.PeopleRepository
+import com.sw.sw_api_kotlin_project.repository.PlanetRepository
 
 /**
  * 検索画面
@@ -15,6 +20,20 @@ class SearchFragment : BaseFragment() {
     private var _binding: FragmentSearchBinding? = null
     private val binding get() = checkNotNull(_binding)
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(
+            this,
+            SearchViewModelFactory(
+                PeopleRepository(SWServiceClient.getService()),
+                FilmsRepository(SWServiceClient.getService()),
+                PlanetRepository(SWServiceClient.getService()),
+            )
+        )[SearchViewModel::class.java]
+    }
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -22,6 +41,16 @@ class SearchFragment : BaseFragment() {
     ): View {
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun initView() {
+        super.initView()
+
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
     }
 
 }
--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/ui/search/SearchViewModel.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/ui/search/SearchViewModel.kt
@@ -3,16 +3,27 @@ package com.sw.sw_api_kotlin_project.ui.search
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.sw.sw_api_kotlin_project.base.BaseViewModel
+import com.sw.sw_api_kotlin_project.repository.FilmsRepository
+import com.sw.sw_api_kotlin_project.repository.PeopleRepository
+import com.sw.sw_api_kotlin_project.repository.PlanetRepository
 
-class SearchViewModel : BaseViewModel() {
+class SearchViewModel(
+    private val peopleRepository: PeopleRepository,
+    private val filmsRepository: FilmsRepository,
+    private val planetRepository: PlanetRepository,
+) : BaseViewModel() {
 
 }
 
-class SearchViewModelFactory() : ViewModelProvider.Factory {
+class SearchViewModelFactory(
+    private val peopleRepository: PeopleRepository,
+    private val filmsRepository: FilmsRepository,
+    private val planetRepository: PlanetRepository,
+) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(SearchViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return SearchViewModel() as T
+            return SearchViewModel(peopleRepository, filmsRepository, planetRepository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
