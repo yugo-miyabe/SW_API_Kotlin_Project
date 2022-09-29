d20426542cb8735e975ecd134cc03de67b560a6c
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-09-29 23:20:15 +0900

    スクロールするよう変更

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/ui/search/SearchFragment.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/ui/search/SearchFragment.kt
@@ -6,20 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.NavDirections
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.sw.sw_api_kotlin_project.adapters.FilmsAdapter
 import com.sw.sw_api_kotlin_project.adapters.PeopleAdapter
+import com.sw.sw_api_kotlin_project.adapters.PlanetAdapter
 import com.sw.sw_api_kotlin_project.api.SWServiceClient
 import com.sw.sw_api_kotlin_project.api.liveData.SWApiLiveDataObserver
 import com.sw.sw_api_kotlin_project.base.BaseFragment
+import com.sw.sw_api_kotlin_project.data.model.Films
 import com.sw.sw_api_kotlin_project.data.model.People
+import com.sw.sw_api_kotlin_project.data.model.Planet
 import com.sw.sw_api_kotlin_project.data.model.Results
 import com.sw.sw_api_kotlin_project.databinding.FragmentSearchBinding
 import com.sw.sw_api_kotlin_project.repository.FilmsRepository
 import com.sw.sw_api_kotlin_project.repository.PeopleRepository
 import com.sw.sw_api_kotlin_project.repository.PlanetRepository
-import com.sw.sw_api_kotlin_project.ui.people.PeopleListFragmentDirections
 
 /**
  * 検索画面
@@ -63,17 +64,27 @@ class SearchFragment : BaseFragment() {
     private fun getSearchResult(searchString: String) {
         val searchResultObserver = object : SWApiLiveDataObserver<List<Results<out Parcelable>>>() {
 
+            @Suppress("UNCHECKED_CAST")
             override fun onSuccess(data: List<Results<out Parcelable>>?) {
                 binding.progressBar.visibility = View.GONE
-
                 val peopleResults: List<People> = data!![0].results as List<People>
                 binding.peopleRecyclerView.adapter = PeopleAdapter(peopleResults) {
-
+                    // TODO 画面遷移処理追加
                 }
                 binding.peopleRecyclerView.layoutManager = LinearLayoutManager(context)
-
-
-
+                val filmsResults: List<Films> = data[1].results as List<Films>
+                binding.filmRecyclerView.adapter = FilmsAdapter(filmsResults) {
+                    // TODO 画面遷移処理追加
+                }
+                binding.filmRecyclerView.layoutManager = LinearLayoutManager(context)
+                val planetResults: List<Planet> = data[2].results as List<Planet>
+                binding.planetRecyclerView.adapter = PlanetAdapter(planetResults) {
+                    // TODO 画面遷移処理追加
+                }
+                binding.planetRecyclerView.layoutManager = LinearLayoutManager(context)
+                binding.peopleRecyclerView.visibility = View.VISIBLE
+                binding.filmRecyclerView.visibility = View.VISIBLE
+                binding.planetRecyclerView.visibility = View.VISIBLE
             }
 
             override fun onError(errorMessage: String) {
@@ -83,6 +94,9 @@ class SearchFragment : BaseFragment() {
             override fun onLoading() {
                 super.onLoading()
                 binding.progressBar.visibility = View.VISIBLE
+                binding.peopleRecyclerView.visibility = View.GONE
+                binding.filmRecyclerView.visibility = View.GONE
+                binding.planetRecyclerView.visibility = View.GONE
             }
 
         }
