abc6a7e4c6ca55399a6367275441d3afc1452e8b
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-09-29 10:04:00 +0900

    登場人物の検索APIをUIから叩く

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/ui/search/SearchFragment.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/ui/search/SearchFragment.kt
@@ -6,7 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import com.sw.sw_api_kotlin_project.api.SWServiceClient
+import com.sw.sw_api_kotlin_project.api.liveData.SWApiLiveDataObserver
 import com.sw.sw_api_kotlin_project.base.BaseFragment
+import com.sw.sw_api_kotlin_project.data.model.People
+import com.sw.sw_api_kotlin_project.data.model.Results
 import com.sw.sw_api_kotlin_project.databinding.FragmentSearchBinding
 import com.sw.sw_api_kotlin_project.repository.FilmsRepository
 import com.sw.sw_api_kotlin_project.repository.PeopleRepository
@@ -19,7 +22,6 @@ class SearchFragment : BaseFragment() {
     private lateinit var viewModel: SearchViewModel
     private var _binding: FragmentSearchBinding? = null
     private val binding get() = checkNotNull(_binding)
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,6 +48,31 @@ class SearchFragment : BaseFragment() {
     override fun initView() {
         super.initView()
 
+        binding.searchButton.setOnClickListener {
+            val searchString = binding.searchBar.text.toString()
+            getSearchResult(searchString)
+        }
+    }
+
+    private fun getSearchResult(searchString: String) {
+        val searchResultObserver = object : SWApiLiveDataObserver<Results<People>>() {
+
+            override fun onSuccess(data: Results<People>?) {
+                val hoge = data
+            }
+
+            override fun onError(errorMessage: String) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onLoading() {
+                super.onLoading()
+                TODO("Not yet implemented")
+            }
+
+        }
+        viewModel.getSearchResult(searchString = searchString)
+            .observe(viewLifecycleOwner, searchResultObserver)
     }
 
     override fun onDestroy() {
--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/ui/search/SearchViewModel.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/ui/search/SearchViewModel.kt
@@ -2,16 +2,31 @@ package com.sw.sw_api_kotlin_project.ui.search
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.liveData
 import com.sw.sw_api_kotlin_project.base.BaseViewModel
 import com.sw.sw_api_kotlin_project.repository.FilmsRepository
 import com.sw.sw_api_kotlin_project.repository.PeopleRepository
 import com.sw.sw_api_kotlin_project.repository.PlanetRepository
+import com.sw.sw_api_kotlin_project.utils.Resource
+import kotlinx.coroutines.Dispatchers
 
 class SearchViewModel(
     private val peopleRepository: PeopleRepository,
     private val filmsRepository: FilmsRepository,
     private val planetRepository: PlanetRepository,
 ) : BaseViewModel() {
+
+    fun getSearchResult(searchString: String) = liveData(Dispatchers.IO) {
+
+        try {
+            val searchResponse = peopleRepository.getPeopleSearch(search = searchString)
+            //val filmsResponse = filmsRepository.getFilmsSearch(search = searchString)
+            //val planetResponse = planetRepository.getPlanetsSearch(search = searchString)
+            emit(Resource.success(searchResponse))
+        } catch (e: Exception) {
+            emit(Resource.error(data = null, message = e.message ?: "error"))
+        }
+    }
 
 }
 
