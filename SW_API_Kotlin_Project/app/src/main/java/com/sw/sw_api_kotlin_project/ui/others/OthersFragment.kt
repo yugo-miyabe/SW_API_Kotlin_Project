9d1d771d781459c53b3533b63ea5acc3fc33cbc6
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-09-30 00:23:57 +0900

    その他画面追加

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/ui/others/OthersFragment.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/ui/others/OthersFragment.kt
@@ -1,7 +1,38 @@
 package com.sw.sw_api_kotlin_project.ui.others
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.sw.sw_api_kotlin_project.base.BaseFragment
+import com.sw.sw_api_kotlin_project.databinding.FragmentOthersBinding
 
 class OthersFragment : BaseFragment() {
+    private lateinit var viewModel: OthersViewModel
+    private var _binding: FragmentOthersBinding? = null
+    private val binding get() = checkNotNull(_binding)
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(
+            this,
+            OthersViewModelFactory()
+        )[OthersViewModel::class.java]
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentOthersBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
 }
