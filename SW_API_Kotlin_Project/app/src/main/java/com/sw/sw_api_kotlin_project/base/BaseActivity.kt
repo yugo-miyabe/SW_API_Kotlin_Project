58f9a87b00442d07f7f45056b734d16b95e32e9f
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-09-30 22:09:07 +0900

    不要なプロパティを削除

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/base/BaseActivity.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/base/BaseActivity.kt
@@ -1,12 +1,9 @@
 package com.sw.sw_api_kotlin_project.base
 
-import android.app.Dialog
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseActivity : AppCompatActivity() {
-
-    private var mProgressDialog: Dialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
