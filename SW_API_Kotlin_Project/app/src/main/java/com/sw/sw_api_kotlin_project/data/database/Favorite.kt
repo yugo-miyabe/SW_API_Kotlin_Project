35a7bedd4c6a99d926ee77341808b0dbbaa1ecd4
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-10-02 23:02:42 +0900

    TODOコメントを追加

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/data/database/Favorite.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/data/database/Favorite.kt
@@ -4,6 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.sw.sw_api_kotlin_project.data.model.People
 
+//TODO ジェネリクス型に変更
 @Entity(tableName = "favorite_table")
 data class Favorite(
     @PrimaryKey(autoGenerate = true)
