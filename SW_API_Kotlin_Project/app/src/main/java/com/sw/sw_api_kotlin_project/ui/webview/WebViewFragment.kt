d7c2d526d76d714c03bea4f250942cd3a0b1ef24
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-10-08 22:41:05 +0900

    WebViewをセットする処理を追加

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/ui/webview/WebViewFragment.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/ui/webview/WebViewFragment.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.sw.sw_api_kotlin_project.base.BaseFragment
 import com.sw.sw_api_kotlin_project.databinding.FragmentWebViewBinding
 
 class WebViewFragment : BaseFragment() {
     private var _binding: FragmentWebViewBinding? = null
     private val binding get() = checkNotNull(_binding)
-
+    private val args: WebViewFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,7 +24,8 @@ class WebViewFragment : BaseFragment() {
 
     override fun initView() {
         super.initView()
-        
+        val webViewUrl = args.url
+        binding.webView.loadUrl(webViewUrl)
     }
 
     override fun onDestroy() {
