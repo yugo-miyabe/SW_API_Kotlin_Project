93da16e35b815f1c3b26c9c29d2ea68624bc8fbe
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-10-11 13:41:53 +0900

    名前付き引数を削除

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/repository/FavoriteRepository.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/repository/FavoriteRepository.kt
@@ -5,11 +5,11 @@ import com.sw.sw_api_kotlin_project.data.database.FavoriteDao
 
 class FavoriteRepository(private val favoriteDao: FavoriteDao) {
 
-    suspend fun insert(favorite: Favorite) = favoriteDao.insert(favorite = favorite)
+    suspend fun insert(favorite: Favorite) = favoriteDao.insert(favorite)
 
-    suspend fun delete(favorite: Favorite) = favoriteDao.delete(favorite = favorite)
+    suspend fun delete(favorite: Favorite) = favoriteDao.delete(favorite)
 
-    suspend fun getFavoriteState(name: String) = favoriteDao.getFavorite(name = name)
+    suspend fun getFavoriteState(name: String) = favoriteDao.getFavorite(name)
 
     /**
      * 全て取得
