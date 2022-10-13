f4babf6d62b7ded36ea57c98ef1bda6fa0784475
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-10-13 21:41:48 +0900

    ファイル追加

---
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/data/FavoriteLocalDatasource.kt
@@ -0,0 +1,20 @@
+package com.sw.sw_api_kotlin_project.data
+
+import com.sw.sw_api_kotlin_project.data.database.Favorite
+import com.sw.sw_api_kotlin_project.data.database.FavoriteDao
+import javax.inject.Inject
+
+class FavoriteLocalDatasource @Inject constructor(private val favoriteDao: FavoriteDao) :
+    FavoriteDatasource {
+
+    override suspend fun insert(favorite: Favorite) {
+        favoriteDao.insert(favorite)
+    }
+
+    override suspend fun delete(favorite: Favorite) {
+        favoriteDao.delete(favorite)
+    }
+
+    override suspend fun isFavoriteExist(name: String): Favorite? = favoriteDao.getFavorite(name)
+
+}
