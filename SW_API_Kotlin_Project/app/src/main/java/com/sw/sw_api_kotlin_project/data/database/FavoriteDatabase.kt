9835a4c35a87002c5122065ae5395b06d670e08e
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-10-14 15:21:28 +0900

    データベースのインスタンスを取得する処理を削除

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/data/database/FavoriteDatabase.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/data/database/FavoriteDatabase.kt
@@ -1,8 +1,6 @@
 package com.sw.sw_api_kotlin_project.data.database
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
@@ -10,20 +8,4 @@ import androidx.room.TypeConverters
 @TypeConverters(Converters::class)
 abstract class FavoriteDatabase : RoomDatabase() {
     abstract fun favoriteDao(): FavoriteDao
-
-    companion object {
-        private var instance: FavoriteDatabase? = null
-
-        fun getDatabase(context: Context): FavoriteDatabase {
-            if (instance == null) {
-                instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    FavoriteDatabase::class.java,
-                    "favorite"
-                ).build()
-            }
-            return instance!!
-        }
-    }
-
 }
