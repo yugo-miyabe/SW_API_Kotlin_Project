b9fc8c3e56874d437da3b5a657dc5df9bb1beff2
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-10-16 01:18:40 +0900

    Activity追加

---
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/activity/planet/PlanetActivity.kt
@@ -0,0 +1,12 @@
+package com.sw.sw_api_kotlin_project.activity.planet
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sw.sw_api_kotlin_project.R
+
+class PlanetActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_planet)
+    }
+}
