b2d2cb7aee41ac048bab30828c4186bbb3ba8a9c
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-10-16 16:19:50 +0900

    リネーム

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/activity/planet/PlanetDetailsActivity.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/activity/planet/PlanetDetailsActivity.kt
@@ -12,7 +12,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class PlanetDetailsActivity : AppCompatActivity(), PlanetNavListener {
     private var _binding: ActivityPlanetDetailsBinding? = null
     private val binding get() = checkNotNull(_binding)
-    private val args: PlanetActivityArgs by navArgs()
+    private val args: PlanetDetailsActivityArgs by navArgs()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
