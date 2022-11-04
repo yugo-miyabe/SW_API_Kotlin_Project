3e9d60abe2c47324c2cb17540781e1cd94af1b1b
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-11-04 18:16:58 +0900

    新規追加したデータベースを定義

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/database/FavoriteDao.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/database/FavoriteDao.kt
@@ -5,15 +5,35 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import com.sw.sw_api_kotlin_project.model.entity.Favorite
-import kotlinx.coroutines.flow.Flow
+import com.sw.sw_api_kotlin_project.model.entity.FavoriteFilm
+import com.sw.sw_api_kotlin_project.model.entity.FavoritePeople
+import com.sw.sw_api_kotlin_project.model.entity.FavoritePlanet
 
 @Dao
 interface FavoriteDao {
     @Insert
     suspend fun insert(favorite: Favorite)
 
+    @Insert
+    suspend fun insertPeople(people: FavoritePeople)
+
+    @Insert
+    suspend fun insertFilm(film: FavoriteFilm)
+
+    @Insert
+    suspend fun insertPlanet(planet: FavoritePlanet)
+
     @Delete
     suspend fun delete(favorite: Favorite)
+
+    @Delete
+    suspend fun deletePeople(people: FavoritePeople)
+
+    @Delete
+    suspend fun deleteFilm(film: FavoriteFilm)
+
+    @Delete
+    suspend fun deletePlanet(planet: FavoritePlanet)
 
     @Query("SELECT * FROM favorite_table WHERE name = :name LIMIT 1")
     suspend fun getFavorite(name: String): Favorite?
--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/database/FavoriteDatabase.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/database/FavoriteDatabase.kt
@@ -4,8 +4,15 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.sw.sw_api_kotlin_project.model.entity.Favorite
+import com.sw.sw_api_kotlin_project.model.entity.FavoriteFilm
+import com.sw.sw_api_kotlin_project.model.entity.FavoritePeople
+import com.sw.sw_api_kotlin_project.model.entity.FavoritePlanet
 
-@Database(entities = [Favorite::class], version = 1, exportSchema = false)
+@Database(
+    entities = [Favorite::class, FavoritePeople::class, FavoriteFilm::class, FavoritePlanet::class],
+    version = 1,
+    exportSchema = false
+)
 @TypeConverters(Converters::class)
 abstract class FavoriteDatabase : RoomDatabase() {
     abstract fun favoriteDao(): FavoriteDao
