b07b884663c589ed66dac50f001359069169bbe0
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-12-05 17:47:25 +0900

    PeoplePagingSource追加

---
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/model/PeoplePagingSource.kt
@@ -0,0 +1,40 @@
+package com.sw.sw_api_kotlin_project.model
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.sw.sw_api_kotlin_project.network.SWService
+import com.sw.sw_api_kotlin_project.network.model.People
+import okio.IOException
+
+private const val FIRST_PAGE_KEY = 1
+
+class PeoplePagingSource(
+    private val swService: SWService,
+) : PagingSource<Int, People>() {
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, People> {
+        val position: Int = params.key ?: FIRST_PAGE_KEY
+
+        return try {
+            val response = swService.getPeople(position)
+            val nextKey = if (response.next == null) null else position + 1
+            LoadResult.Page(
+                data = response.results,
+                prevKey = if (position == FIRST_PAGE_KEY) null else position - 1,
+                nextKey = nextKey
+            )
+        } catch (exception: IOException) {
+            return LoadResult.Error(exception)
+        } catch (exception: IOException) {
+            return LoadResult.Error(exception)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, People>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
+        }
+    }
+
+}
