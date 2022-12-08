b8a9bd5fc0aaf549c6a44f56719743e15dc15a94
yugo miyabe <yuni42yuyuyu@gmail.com>
2022-12-08 15:06:53 +0900

    Filmのpagingの設定追加

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/model/data/FilmPagingSource.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/model/data/FilmPagingSource.kt
@@ -3,18 +3,17 @@ package com.sw.sw_api_kotlin_project.model.data
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.sw.sw_api_kotlin_project.network.SWService
-import com.sw.sw_api_kotlin_project.network.model.People
+import com.sw.sw_api_kotlin_project.network.model.Film
 import com.sw.sw_api_kotlin_project.network.model.Results
 import okio.IOException
 
-
-class PeoplePagingSource(private val swService: SWService) : PagingSource<Int, People>() {
+class FilmPagingSource(private val swService: SWService) : PagingSource<Int, Film>() {
     private val firstPageKey = 1
 
-    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, People> {
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Film> {
         val position: Int = params.key ?: firstPageKey
         return try {
-            val response: Results<People> = swService.getPeople(position)
+            val response: Results<Film> = swService.getFilms(position)
             val nextKey = if (response.next == null) null else position + 1
             LoadResult.Page(
                 data = response.results,
@@ -28,7 +27,8 @@ class PeoplePagingSource(private val swService: SWService) : PagingSource<Int, P
         }
     }
 
-    override fun getRefreshKey(state: PagingState<Int, People>): Int? {
+
+    override fun getRefreshKey(state: PagingState<Int, Film>): Int? {
         return state.anchorPosition?.let { anchorPosition ->
             state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
                 ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
