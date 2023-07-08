160bc9c6885085215bc282f22a4fed11fd2ede02
yugo miyabe <yuni42yuyuyu@gmail.com>
2023-07-09 00:41:32 +0900

    Baseの追加

---
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/screen/base/BaseFragmentTest.kt
@@ -0,0 +1,67 @@
+package com.sw.sw_api_kotlin_project.screen.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.CallSuper
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavDirections
+import androidx.navigation.fragment.findNavController
+import androidx.viewbinding.ViewBinding
+import com.sw.sw_api_kotlin_project.extnsions.collectIn
+
+abstract class BaseFragmentTest<VM : BaseViewModelTest, B : ViewBinding> : Fragment() {
+
+    abstract val viewModel: VM
+    private var _binding: B? = null
+    val binding: B
+        get() = checkNotNull(_binding) {
+            "bindingはonCreateView - onDestroyView間でのみアクセス可能"
+        }
+
+    abstract fun inflate(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+    ): B
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = inflate(inflater, container)
+        return binding.root
+    }
+
+    @CallSuper
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.uiEventList.collectIn(viewLifecycleOwner) { uiEvents ->
+            uiEvents.forEach { event ->
+                when (event) {
+                    is BaseViewModelTest.UiEvent.Navigate -> {
+
+                    }
+
+                    is BaseViewModelTest.UiEvent.Dialog -> {
+
+                    }
+
+                    is BaseViewModelTest.UiEvent.ToastMessage -> {
+
+                    }
+                }
+            }
+        }
+    }
+
+    private fun navigateTo(actionId: NavDirections) {
+        findNavController().navigate(actionId)
+    }
+
+    @CallSuper
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
---
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/screen/base/BaseViewModelTest.kt
@@ -0,0 +1,32 @@
+package com.sw.sw_api_kotlin_project.screen.base
+
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavDirections
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+
+abstract class BaseViewModelTest : ViewModel() {
+    sealed interface UiEvent {
+        data class Navigate(val navDirections: NavDirections) : UiEvent
+        data class Dialog(val isDoShowing: Boolean) : UiEvent
+        data class ToastMessage(val message: String) : UiEvent
+    }
+
+    private val _uiEventList = MutableStateFlow<List<UiEvent>>(emptyList())
+    val uiEventList: StateFlow<List<UiEvent>> = _uiEventList
+
+    fun addNavigationEvent(destination: NavDirections) {
+        _uiEventList.update { uiEventList ->
+            uiEventList + UiEvent.Navigate(destination)
+        }
+    }
+
+    fun consumeEvent(target: UiEvent) {
+        _uiEventList.update { uiEventList ->
+            uiEventList.filterNot { uiEvent ->
+                uiEvent == target
+            }
+        }
+    }
+}
