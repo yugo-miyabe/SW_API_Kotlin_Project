6c1b166669472e9653a3981621a53b4f1d26dbef
yugo miyabe <yuni42yuyuyu@gmail.com>
2023-07-09 12:28:49 +0900

    PlanetList

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/screen/planet/list/PlanetListFragment.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/screen/planet/list/PlanetListFragment.kt
@@ -6,16 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.paging.LoadState
 import com.google.android.material.appbar.MaterialToolbar
 import com.sw.sw_api_kotlin_project.R
 import com.sw.sw_api_kotlin_project.databinding.FragmentPlanetListBinding
-import com.sw.sw_api_kotlin_project.screen.base.BaseFragment
+import com.sw.sw_api_kotlin_project.screen.base.BaseFragmentTest
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -23,43 +22,33 @@ import kotlinx.coroutines.launch
  * 惑星一覧画面
  */
 @AndroidEntryPoint
-class PlanetListFragment : BaseFragment() {
-    private val viewModel: PlanetViewModel by viewModels()
-    private var _binding: FragmentPlanetListBinding? = null
-    private val binding get() = checkNotNull(_binding)
+class PlanetListFragment : BaseFragmentTest<PlanetViewModel, FragmentPlanetListBinding>() {
+    override val viewModel: PlanetViewModel by viewModels()
 
-    override fun onCreateView(
+    override fun inflate(
         inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentPlanetListBinding.inflate(inflater, container, false)
-        return binding.root
-    }
+        container: ViewGroup?
+    ): FragmentPlanetListBinding = FragmentPlanetListBinding.inflate(inflater, container, false)
 
-    override fun initView() {
-        super.initView()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         binding.planetListAppbar.findViewById<MaterialToolbar>(R.id.toolbar).apply {
             setOnClickListener {
                 findNavController().popBackStack()
             }
             title = getString(R.string.planet_title)
         }
-    }
 
-    override fun addObservers() {
-        super.addObservers()
 
         val adapter = PlanetListAdapter {
             val action = PlanetListFragmentDirections.actionNavPlanetToNavPlanetDetail(it)
             findNavController().navigate(action)
         }
 
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.planetItems.collect {
-                    adapter.submitData(it)
-                }
+        viewModel.viewModelScope.launch {
+            viewModel.planetItems.collect {
+                adapter.submitData(it)
             }
         }
 
@@ -74,17 +63,9 @@ class PlanetListFragment : BaseFragment() {
                     binding.errorText.text = loadStateRefresh.error.localizedMessage
             }
         }
-
         binding.retryButton.setOnClickListener {
             adapter.retry()
         }
-
         binding.planetRecyclerView.adapter = adapter
-
-    }
-
-    override fun onDestroy() {
-        _binding = null
-        super.onDestroy()
     }
 }
--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/screen/planet/list/PlanetViewModel.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/screen/planet/list/PlanetViewModel.kt
@@ -11,6 +11,7 @@ import com.sw.sw_api_kotlin_project.data.model.repository.PlanetRepository
 import com.sw.sw_api_kotlin_project.data.network.model.Planet
 import com.sw.sw_api_kotlin_project.data.network.model.Results
 import com.sw.sw_api_kotlin_project.screen.base.BaseViewModel
+import com.sw.sw_api_kotlin_project.screen.base.BaseViewModelTest
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -18,7 +19,7 @@ import javax.inject.Inject
 @HiltViewModel
 class PlanetViewModel @Inject constructor(
     private val planetRepository: PlanetRepository
-) : BaseViewModel() {
+) : BaseViewModelTest() {
     private val _planetList = MutableLiveData<Results<Planet>>()
     val planetList: LiveData<Results<Planet>> get() = _planetList
     private val _isLoading = MutableLiveData<Boolean>()
