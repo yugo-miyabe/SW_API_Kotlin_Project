ebc811ccbeb8c295c9569cbd71c665adac3159ea
yugo miyabe <yuni42yuyuyu@gmail.com>
2023-07-09 11:24:49 +0900

    FilmList修正

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/screen/film/list/FilmListFragment.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/screen/film/list/FilmListFragment.kt
@@ -6,16 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.paging.LoadState
 import com.google.android.material.appbar.MaterialToolbar
 import com.sw.sw_api_kotlin_project.R
 import com.sw.sw_api_kotlin_project.databinding.FragmentFilmListBinding
-import com.sw.sw_api_kotlin_project.screen.base.BaseFragment
+import com.sw.sw_api_kotlin_project.screen.base.BaseFragmentTest
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
@@ -24,42 +23,28 @@ import kotlinx.coroutines.launch
  * 映画一覧画面
  */
 @AndroidEntryPoint
-class FilmListFragment : BaseFragment() {
-    private val viewModel: FilmListViewModel by viewModels()
-    private var _binding: FragmentFilmListBinding? = null
-    private val binding get() = checkNotNull(_binding)
+class FilmListFragment : BaseFragmentTest<FilmListViewModel, FragmentFilmListBinding>() {
+    override val viewModel: FilmListViewModel by viewModels()
+    override fun inflate(inflater: LayoutInflater, container: ViewGroup?): FragmentFilmListBinding =
+        FragmentFilmListBinding.inflate(inflater, container, false)
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentFilmListBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun initView() {
-        super.initView()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         binding.filmListAppbar.findViewById<MaterialToolbar>(R.id.toolbar).apply {
             setOnClickListener {
                 findNavController().popBackStack()
             }
             title = getString(R.string.film_title)
         }
-    }
 
-    override fun addObservers() {
-        super.addObservers()
         val adapter = FilmListAdapter {
             val action = FilmListFragmentDirections.actionNavFilmsToNavFilmsDetail(it)
             findNavController().navigate(action)
         }
 
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.filmItems.collectLatest {
-                    adapter.submitData(it)
-                }
+        viewModel.viewModelScope.launch {
+            viewModel.filmItems.collectLatest {
+                adapter.submitData(it)
             }
         }
 
@@ -70,22 +55,15 @@ class FilmListFragment : BaseFragment() {
                 binding.progressBar.isVisible = loadStateRefresh is LoadState.Loading
                 binding.errorText.isVisible = loadStateRefresh is LoadState.Error
                 binding.retryButton.isVisible = loadStateRefresh is LoadState.Error
-                if (loadStateRefresh is LoadState.Error)
-                    binding.errorText.text = loadStateRefresh.error.localizedMessage
+                if (loadStateRefresh is LoadState.Error) binding.errorText.text =
+                    loadStateRefresh.error.localizedMessage
             }
         }
-        
+
         binding.retryButton.setOnClickListener {
             adapter.retry()
         }
 
         binding.filmRecyclerView.adapter = adapter
-
-    }
-
-
-    override fun onDestroy() {
-        _binding = null
-        super.onDestroy()
     }
 }
--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/screen/film/list/FilmListViewModel.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/screen/film/list/FilmListViewModel.kt
@@ -1,33 +1,21 @@
 package com.sw.sw_api_kotlin_project.screen.film.list
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
-import com.sw.sw_api_kotlin_project.data.model.entity.PageType
-import com.sw.sw_api_kotlin_project.data.model.entity.RequestStatus
 import com.sw.sw_api_kotlin_project.data.model.repository.FilmRepository
 import com.sw.sw_api_kotlin_project.data.network.model.Film
-import com.sw.sw_api_kotlin_project.data.network.model.Results
-import com.sw.sw_api_kotlin_project.screen.base.BaseViewModel
+import com.sw.sw_api_kotlin_project.screen.base.BaseViewModelTest
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class FilmListViewModel @Inject constructor(
     private val filmRepository: FilmRepository
-) : BaseViewModel() {
-    private val _filmList = MutableLiveData<Results<Film>>()
-    val filmList: LiveData<Results<Film>> get() = _filmList
-    private val _isLoading = MutableLiveData<Boolean>()
-    val isLoading: LiveData<Boolean> get() = _isLoading
-    private val _failureMessage = MutableLiveData<String>()
-    val failureMessage: LiveData<String> get() = _failureMessage
+) : BaseViewModelTest() {
 
     val filmItems: Flow<PagingData<Film>> = Pager(config = PagingConfig(
         pageSize = 1,
@@ -35,26 +23,4 @@ class FilmListViewModel @Inject constructor(
     ), pagingSourceFactory = {
         filmRepository.filmListPagingSource()
     }).flow.cachedIn(viewModelScope)
-
-    fun getFilm(pageType: PageType) {
-        pageParameterFormat(pageType)
-        viewModelScope.launch {
-            filmRepository.getFilm(page).collect { response ->
-                when (response.status) {
-                    RequestStatus.SUCCESS -> {
-                        _isLoading.value = false
-                        _filmList.value = response.data!!
-                    }
-                    RequestStatus.ERROR -> {
-                        _isLoading.value = false
-                        _failureMessage.value = response.message!!
-                    }
-                    RequestStatus.LOADING -> {
-                        _isLoading.value = true
-                        _failureMessage.value = ""
-                    }
-                }
-            }
-        }
-    }
 }
