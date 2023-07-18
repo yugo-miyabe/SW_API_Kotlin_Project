6d98185757dcf90b85a0f29178ae460a47724788
yugo miyabe <yuni42yuyuyu@gmail.com>
2023-07-18 17:37:32 +0900

    たぶの処理追加

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/screen/MainActivity.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/screen/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.sw.sw_api_kotlin_project.R
 import com.sw.sw_api_kotlin_project.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -32,15 +33,40 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
         appBarConfiguration = AppBarConfiguration(
-            setOf(R.id.nav_home_list, R.id.nav_search, R.id.nav_favorite_list, R.id.nav_other)
+            setOf(R.id.home_list, R.id.search, R.id.favorite_list, R.id.other)
         )
 
         setupActionBarWithNavController(navController, appBarConfiguration)
+
+
+        binding.bottomNavigationView.apply {
+            setupWithNavController(navController)
+            setOnItemReselectedListener { item ->
+                val menuItemIdToFragmentIdMap = mapOf(
+                    R.id.nav_home_list to R.id.nav_home_list,
+                    R.id.nav_search to R.id.search,
+                    R.id.nav_favorite_list to R.id.favorite_list,
+                    R.id.nav_other to R.id.other,
+                )
+                val rootDestinationIds = menuItemIdToFragmentIdMap.values
+                val currentId = navController.currentDestination?.id
+                    ?: return@setOnItemReselectedListener
+                val rootId = menuItemIdToFragmentIdMap[item.itemId]
+                    ?: return@setOnItemReselectedListener
+
+                if (currentId in rootDestinationIds) {
+                    // 親画面だった時の処理
+                    return@setOnItemReselectedListener
+                } else {
+                    // 子画面だった時の処理
+                    navController.popBackStack(rootId, false)
+                }
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp(appBarConfiguration) ||
-                super.onSupportNavigateUp()
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
     override fun onDestroy() {
