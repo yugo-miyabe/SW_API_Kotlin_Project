a8654999de5c338ebfde3c5327aaa6e6ac3bbf7f
yugo miyabe <yuni42yuyuyu@gmail.com>
2023-07-18 16:54:10 +0900

    AppBar修正

--- SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/screen/MainActivity.kt
+++ SW_API_Kotlin_Project/app/src/main/java/com/sw/sw_api_kotlin_project/screen/MainActivity.kt
@@ -2,14 +2,19 @@ package com.sw.sw_api_kotlin_project.screen
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.sw.sw_api_kotlin_project.R
 import com.sw.sw_api_kotlin_project.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
+
     private var _binding: ActivityMainBinding? = null
     private val binding get() = checkNotNull(_binding)
 
@@ -19,9 +24,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
 
+        /*
         val bottomNavigationView = binding.bottomNavigationView
         val navController = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
         bottomNavigationView.setupWithNavController((navController!!.findNavController()))
+        */
+
+        setUpNavigation()
+    }
+
+    private fun setUpNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.nav_home_list, R.id.nav_search, R.id.nav_favorite_list, R.id.nav_other)
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
     override fun onDestroy() {
